93e802bd0b7647aa49d1075a38f97494a2505941
Wellington  Carvalho de Oliveira <wellingtondevops@gmail.com>
2021-01-19 17:09:10 -0300

    aula 49 finalizada

--- node-mongo-compose/backend/app.js
+++ node-mongo-compose/backend/app.js
@@ -2,37 +2,29 @@ const express = require('express')
 const restful = require('node-restful')
 const server = express()
 const mongoose = restful.mongoose
-// const bodyParser = require('body-parser')
-// const cors = require('cors')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // Database
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://db/mydb')
 
-//Teste
+// Middlewares
+server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.json())
+server.use(cors())
 
-server.get('/',(req,res,next)=>{
-    res.send('Backend')
+// ODM
+const Client = restful.model('Client', {
+    name: { type: String, required: true }
 })
 
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runValidators: true})
 
+// Routes
+Client.register(server, '/clients')
 
-// // Middlewares
-// server.use(bodyParser.urlencoded({extended:true}))
-// server.use(bodyParser.json())
-// server.use(cors())
-
-// // ODM
-// const Client = restful.model('Client', {
-//     name: { type: String, required: true }
-// })
-
-// // Rest API
-// Client.methods(['get', 'post', 'put', 'delete'])
-// Client.updateOptions({new: true, runValidators: true})
-
-// // Routes
-// Client.register(server, '/clients')
-
-// // Start Server
- server.listen(3000)
+// Start Server
+server.listen(3000)
